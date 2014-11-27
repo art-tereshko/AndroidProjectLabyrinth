58c57a174dfd234c6f9c79bd135cdbac46aea915
KoksRS <artem_tereshko@hotmail.com>
2014-11-27 19:34:08 +0100

    Version de base 27nov

--- app/src/main/java/game/Bullet.java
+++ app/src/main/java/game/Bullet.java
@@ -1,11 +1,10 @@
 package game;
 
-/**
- * Created by Artem Tereshko on 11/11/2014.
- */
+
 public class Bullet extends Ball {
 
     public Bullet(int height, int width) {
         super(height, width);
     }
+    
 }
